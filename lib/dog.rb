40f2d6223d776d00253b408d2be93f1889ca962f
Uma Desai <umadesai38@gmail.com>
2018-07-12 22:27:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,23 @@
 class Dog 
-  def initalize(name)
+  def initalize(name,breed)
   @name = name 
+  @breed = breed
 end
 
 def name 
-  @name = "Bailey"
+  @name = "Fido"
 end
 
 def name=(new_name)
   @name = new_name
 end
 
+def breed
+  @breed = "Beagle"
+end
+
+def breed=(new_breed)
+  @breed = new_breed 
+end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# person.rb
+class Person
+  def initalize(name,job)
+  @name = name 
+  @job = job
+end
+
+def name 
+  @name = "Beyonce"
+end
+
+def name=(new_name)
+  @name = new_name
+end
+
+def job
+  @job = "Singer"
+end
+
+def job=(new_job)
+  @job = new_job
+end
+
+end
